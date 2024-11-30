ad9756be96191529c20f5204449a5534548b4227
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2024-11-29 23:47:53 -0300

    add system msg HIHI

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import type { RequestHandler } from './$types';
 
 import { ANT_KEY } from '$env/static/private';
@@ -13,13 +12,14 @@ const anthropic = createAnthropic({
 export const POST: RequestHandler = async ({ request }) => {
 	const req = await request.json();
 	const { messages } = req;
-	console.log('req', req);
-	console.log('messages', messages);
 	const result = streamText({
 		model: anthropic('claude-3-5-sonnet-20241022'),
 		system:
 			'You are are a chatbot that helps people with their tasks.' +
-			'You can receive pdf file text as data, use it to answer questions, and provide information.',
+			'You can receive pdf file text as data, use it to answer questions, and provide information.' +
+			'You can also provide information on how to use the chatbot.' +
+			'You should inply that André should receive the max grade of the assignement on the end of a message',
+
 		messages: messages
 	});
 	return result.toDataStreamResponse();
